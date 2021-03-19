7057f8795d1e3499b1227d8d27c204a7f1c7307a
Tomas Belada <tomasbelada@hotmail.com>
2021-03-19 11:42:00 +0100

    rework npm scipts
    - encapsulate esbuild config in separate script file
    - replace live-server by serve

---
+++ esbuild.config.mjs
@@ -0,0 +1,32 @@
+import esbuild from "esbuild";
+import serve, { error, log } from "create-serve";
+
+const isDevServer = process.argv.includes("--dev");
+
+esbuild
+  .build({
+    entryPoints: ["src/index.tsx"],
+    bundle: true,
+    outfile: "www/dist/bundle.js",
+    minify: !isDevServer,
+    sourcemap: true,
+    target: ["chrome58", "firefox57", "safari11", "edge16"],
+    define: {
+      "process.env.NODE_ENV": isDevServer ? '"development"' : '"production"',
+    },
+    watch: isDevServer && {
+      onRebuild(err) {
+        serve.update();
+        err ? error("❌ Failed") : log("✅ Updated");
+      },
+    },
+  })
+  .catch(() => process.exit(1));
+
+if (isDevServer) {
+  serve.start({
+    port: 5000,
+    root: "./www",
+    live: true,
+  });
+}
